d2b6f094f2e7a841b63345a18b003516591539fb
AlexPopov487 <apopov974@gmail.com>
2025-07-27 14:35:14 +0300

    #23 - spikes

---
+++ scripts/spike.gd
@@ -0,0 +1,11 @@
+extends Area2D
+@onready var timer: Timer = $Timer
+@onready var player: CharacterBody2D = %Player
+
+func _on_body_entered(player: CharacterBody2D) -> void:
+	print("Player's out of playing ground! You're dead")
+	player.change_move_type("DEAD")
+
+func _on_area_entered(enemy_attack_area: Area2D) -> void:
+	print("Enemy's out of playing ground and now is dead")
+	enemy_attack_area.get_parent().set_dead()
