0a3155a618f226ecd54c755fb4fef2928d45a2b7
AlexPopov487 <apopov974@gmail.com>
2025-07-27 22:11:08 +0300

    #23 animated trees, flag, wheel

--- scripts/spike.gd
+++ scripts/spike.gd
@@ -3,9 +3,9 @@ extends Area2D
 @onready var player: CharacterBody2D = %Player
 
 func _on_body_entered(player: CharacterBody2D) -> void:
-	print("Player's out of playing ground! You're dead")
+	print("Player's hit spikes! You're dead")
 	player.change_move_type("DEAD")
 
 func _on_area_entered(enemy_attack_area: Area2D) -> void:
-	print("Enemy's out of playing ground and now is dead")
+	print("Enemy's hit spikes and now is dead")
 	enemy_attack_area.get_parent().set_dead()
