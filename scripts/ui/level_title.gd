ff0f720385f2eead6393b0463a9c1bfbb8d1bb76
AlexPopov487 <apopov974@gmail.com>
2025-11-10 22:45:59 +0500

    #44 - add level title

---
+++ scripts/ui/level_title.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+
+const TRANSPARENT: Color = Color(1,1,1,0)
+const OPAQUE: Color = Color(1,1,1,1)
+
+@export var _title: String
+
+@onready var _timer: Timer = $Timer
+@onready var _h_box_container: HBoxContainer = $HBoxContainer
+
+var _tween: Tween
+@onready var _label: Label = $HBoxContainer/PanelContainer/Label
+
+func _ready() -> void:
+	_label.text = _title
+	visible = true
+	_h_box_container.modulate = TRANSPARENT
+	_timer.start(1)
+	await _timer.timeout
+	await _fade_to_opaque()
+	_timer.start(2)
+	await _timer.timeout
+	_timer.start(2)
+	await _fade_to_clear()
+	visible = false
+
+func _fade_to_clear() -> Signal:
+	_stop_running_tween()
+	_tween = create_tween()
+	_tween.tween_property(_h_box_container, "modulate", TRANSPARENT, 1)
+	return _tween.finished
+	
+func _fade_to_opaque() -> Signal:
+	_stop_running_tween()
+	_tween = create_tween()
+	_tween.tween_property(_h_box_container, "modulate", OPAQUE, 1)
+	return _tween.finished
+
+func _stop_running_tween():
+	if _tween && _tween.is_running():
+		_tween.kill()
