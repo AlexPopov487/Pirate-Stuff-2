eba4f980f8cae577efeaccd1acdeb9b9f4f9c619
AlexPopov487 <apopov974@gmail.com>
2025-11-10 21:39:54 +0500

    #43 - level 7 complete

---
+++ scripts/levels/level_7.gd
@@ -0,0 +1,12 @@
+extends Level
+class_name Level7
+
+func _init() -> void:
+	_weather = Globals.WEATHER.CLEAR
+	_time = Globals.TIME.NIGHT
+
+
+func _ready() -> void:
+	super._ready()
+	_player_armed = true
+	_controls_enabled_by_default = true
